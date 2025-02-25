14c9f86f58608daaca78c7177128f41be4e39ad5
Mitel <supermiel@gmail.com>
2025-02-25 23:56:56 +0200

    ran next/codemod for changing image imports

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Link from "next/link";
-import Image from "next/image";
+import Image from "next/legacy/image";
 import React, { useState } from "react";
 import { Transition } from "@headlessui/react";
 import { HiOutlineXMark, HiBars3 } from "react-icons/hi2";
